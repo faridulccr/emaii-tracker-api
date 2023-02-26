8e0e2430a7ca19efa8d343e5769fb3a259585ecb
Md Faridul islam <mdfaridulislamovi@gmail.com>
2023-02-27 03:23:50 +0600

    minor change

--- db.js
+++ db.js
@@ -10,7 +10,7 @@ mongoose
         console.log("mongodb atlas is connected");
     })
     .catch((err) => {
-        // console.log(err);
+        console.log(err);
         console.log("mongoDB is not connected");
         process.exit(1); // to stop server when occurs an error
     });
